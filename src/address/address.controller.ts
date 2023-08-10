d1ab589e22d35647c0a85e1493574230d57d4c0f
tercioab <suaagenciainicial@gmail.com>
2023-08-10 15:49:21 -0300

    feat(controller address): add create method

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Post()
-  create(@Body() createAddressDto: CreateAddressDto) {
+  create(
+    @Body() createAddressDto: CreateAddressDto,
+    @CurrentUser() user: User,
+  ) {
+    createAddressDto.userId = user.id;
     return this.addressService.create(createAddressDto);
   }
 
