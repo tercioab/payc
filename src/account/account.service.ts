314a15d109629c2315abc267d9af0be1617ad4e4
tercioab <suaagenciainicial@gmail.com>
2023-08-11 16:04:16 -0300

    refactore(service account): remove update of service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -13,10 +12,6 @@ export class AccountService {
 
   findOne(id: number) {
     return this.prisma.account.findUnique({ where: { userId: id } });
-  }
-
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
   }
 
   async updateBalance(userId: number, newBalance: number) {
