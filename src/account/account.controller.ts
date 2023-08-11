3a0f625d1b8df14b34a16db6a7a31694f3ad1b90
tercioab <suaagenciainicial@gmail.com>
2023-08-11 11:07:13 -0300

    feat(controller account): method to transfer values

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import { TransferDto } from './dto/transfer.dto';
 
 @Controller('account')
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post()
   create(
@@ -25,8 +39,14 @@ export class AccountController {
   }
 
   @Get()
-  findOne(@CurrentUser() user: User) {
-    return this.accountService.findOne(user.id);
+  async findOne(@CurrentUser() user: User) {
+    const userFinded = await this.accountService.findOne(user.id);
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return userFinded;
   }
 
   @Patch(':id')
@@ -34,16 +54,41 @@ export class AccountController {
     return this.accountService.update(+id, updateAccountDto);
   }
 
-  @Patch(':cpfUser')
-  async transaction(
-    @Param('cpfUser') transaction: string,
-    @CurrentUser() user: User,
-    @Body() updateAccountDto: UpdateAccountDto,
+  @Post('transfer')
+  async performTransfer(
+    @CurrentUser() sender: User,
+    @Body() transferData: TransferDto,
   ) {
-    // buscar user pelo cpf
-    // puxar id do user
-    // verificar o balance do usuario atual
-    // fazer a transação para o outro user
-    // let { balance } = await this.findOne(user);
+    const senderUserAccount = await this.findOne(sender);
+
+    const destinationUser = await this.userService.findByCpf(
+      transferData.destinationCpf,
+    );
+    const destinationUserAccount = await this.findOne(destinationUser);
+
+    if (senderUserAccount.balance < transferData.amount) {
+      throw new HttpException(
+        'Insufficient balance for transfer.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const newSenderBalance = senderUserAccount.balance - transferData.amount;
+    const newDestinationBalance =
+      destinationUserAccount.balance + transferData.amount;
+
+    await Promise.all([
+      this.accountService.updateBalance(sender.id, newSenderBalance),
+      this.accountService.updateBalance(
+        destinationUser.id,
+        newDestinationBalance,
+      ),
+    ]);
+
+    return {
+      transferTo: destinationUser.name,
+      amount: transferData.amount,
+      date: new Date(),
+    };
   }
 }
