774b570ed0b90838d58807fa26f9d55d0fb1eafa
tercioab <suaagenciainicial@gmail.com>
2023-08-12 19:03:08 -0300

    feat(module account): add historicService on providers

--- src/historic/dto/create-historic.dto.ts
+++ src/historic/dto/create-historic.dto.ts
@@ -1,11 +1,10 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateHistoricDto {
-  transferTo: string;
-  receivedFrom: string;
+  transferTo?: string;
+  receivedFrom?: string;
   @IsNotEmpty()
   amount: number;
-  date: Date;
   @IsNotEmpty()
   userId: number;
 }
