de8c4bf935d5c7be526c8f1f00c89d778b48edd1
tercioab <suaagenciainicial@gmail.com>
2023-08-12 17:54:10 -0300

    feat(dto historic): create dto of historic

--- src/historic/dto/create-historic.dto.ts
+++ src/historic/dto/create-historic.dto.ts
@@ -1,1 +1,11 @@
-export class CreateHistoricDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateHistoricDto {
+  transferTo: string;
+  receivedFrom: string;
+  @IsNotEmpty()
+  amount: number;
+  date: Date;
+  @IsNotEmpty()
+  userId: number;
+}
