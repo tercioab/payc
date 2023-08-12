fe1c0ccfdb2e8ba45ec9a12a40479a25b25ab06f
tercioab <suaagenciainicial@gmail.com>
2023-08-12 17:56:14 -0300

    feat(module historic): add prismaModule

--- src/historic/historic.module.ts
+++ src/historic/historic.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HistoricService } from './historic.service';
 import { HistoricController } from './historic.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [HistoricController],
-  providers: [HistoricService]
+  providers: [HistoricService],
 })
 export class HistoricModule {}
