f4ba482c39ffdf6699cc155ba2241bc672c422cb
tercioab <suaagenciainicial@gmail.com>
2023-08-14 15:11:48 -0300

    eslint: project in eslint rules

--- client/src/components/inputPassword.tsx
+++ client/src/components/inputPassword.tsx
@@ -1,23 +1,31 @@
-'use client'
+"use client";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
-import { useState } from 'react'
+import {
+	Button,
+	FormControl,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputRightElement,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function InputPassword(){
-    const [showPassword, setShowPassword] = useState(false)
-    return (
-        <FormControl id="password" isRequired>
-        <FormLabel>Password</FormLabel>
-        <InputGroup>
-          <Input type={showPassword ? 'text' : 'password'} />
-          <InputRightElement h={'full'}>
-            <Button
-              variant={'ghost'}
-              onClick={() => setShowPassword((showPassword) => !showPassword)}>
-              {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-    )
+export default function InputPassword() {
+	const [showPassword, setShowPassword] = useState(false);
+	return (
+		<FormControl id='password' isRequired>
+			<FormLabel>Password</FormLabel>
+			<InputGroup>
+				<Input type={showPassword ? "text" : "password"} />
+				<InputRightElement h={"full"}>
+					<Button
+						variant={"ghost"}
+						onClick={() => setShowPassword(showPassword => !showPassword)}
+					>
+						{showPassword ? <ViewIcon /> : <ViewOffIcon />}
+					</Button>
+				</InputRightElement>
+			</InputGroup>
+		</FormControl>
+	);
 }
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,33 +1,40 @@
-import { Flex, Box, Stack, useColorModeValue  } from '@chakra-ui/react';
-import TitleForm from '@/components/titleForm';
-import InputBasic from '@/components/InputBasic';
-import InputPassword from '@/components/inputPassword';
-import ButtonForm from '@/components/ButtonForm';
+import { Flex, Box, Stack, useColorModeValue, Text, Link } from "@chakra-ui/react";
+import TitleForm from "@/components/titleForm";
+import InputBasic from "@/components/InputBasic";
+import InputPassword from "@/components/inputPassword";
+import ButtonForm from "@/components/ButtonForm";
 
 export default function Login() {
-
-  return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <TitleForm title='Sign in to your account' />
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-            <Stack spacing={4}>
-                <InputBasic label="email" type="email" />
-                <InputPassword />
-            <Stack spacing={10}>
-                <ButtonForm title="sign in" />
-            </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-  );
+	return (
+		<Flex
+			minH={"100vh"}
+			align={"center"}
+			justify={"center"}
+			bg={useColorModeValue("gray.50", "gray.800")}
+		>
+			<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+				<TitleForm title='Sign in to your account' />
+				<Box
+					rounded={"lg"}
+					bg={useColorModeValue("white", "gray.700")}
+					boxShadow={"lg"}
+					p={8}
+				>
+					<Stack spacing={4}>
+						<InputBasic label='email' type='email' />
+						<InputPassword />
+						<ButtonForm title='sign in' />
+					</Stack>
+					<Stack pt={6}>
+						<Text align={"center"}>
+							Not a user?{" "}
+							<Link href='/signup' color={"blue.400"}>
+								Sign Up
+							</Link>
+						</Text>
+					</Stack>
+				</Box>
+			</Stack>
+		</Flex>
+	);
 }
