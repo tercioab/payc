856483932ee92ed9b3766367486747de769238c3
tercioab <suaagenciainicial@gmail.com>
2023-08-17 10:24:21 -0300

    refactore: remove context api

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,15 +3,12 @@ import type { AppProps } from "next/app";
 import MainContainer from "@/components/MainContainer";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "@/context/AuthContext";
-import { ApiProvider } from "@/context/ApiContext";
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
 			<MainContainer>
 				<AuthProvider>
-					<ApiProvider>
-						<Component {...pageProps} />
-					</ApiProvider>
+					<Component {...pageProps} />
 				</AuthProvider>
 			</MainContainer>
 		</ChakraProvider>
