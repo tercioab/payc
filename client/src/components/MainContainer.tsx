777062167d32006aaeddc3518d891fc623de9f62
tercioab <suaagenciainicial@gmail.com>
2023-08-17 10:20:58 -0300

    refactore: remove header of maincontainer "

--- client/src/components/MainContainer.tsx
+++ client/src/components/MainContainer.tsx
@@ -1,12 +1,9 @@
 import childrenProps from "@/interface/childrenProps";
-import Header from "./Header";
 
 export default function MainContainer({ children }: childrenProps) {
-    return (
-        <>
-            <Header />
-            <main>{ children }</main>
-            <h2>footer</h2>
-        </>
-    )
+	return (
+		<>
+			<main>{children}</main>
+		</>
+	);
 }
